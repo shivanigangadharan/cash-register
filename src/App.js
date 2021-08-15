dbb5d3f324ef8f07a0bc973e5b06b098b3e9b20f
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-08-16 00:59:41 +0530

    feat: created structure for app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [amt, setAmt] = useState();
+  const [cash, setCash] = useState();
+  const [showCash, setShowCash] = useState(true);
+  const [showTable, setShowTable] = useState(true);
+  function handleNextClick(e) {
+    e.preventDefault();
+    if (amt === undefined) {
+      console.log('amt undefined');
+      setShowCash(true)
+    }
+    else {
+      setShowCash(false);
+    }
+  }
+  function handleCheckClick(e) {
+    e.preventDefault();
+    if (cash === undefined) {
+      setShowTable(true)
+    }
+    else {
+      setShowTable(false);
+    }
+  }
+  function handleAmtChange(e) {
+    setAmt(e);
+  }
+  function handleCashChange(e) {
+    setCash(e);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Cash register manager </h1>
+      <p> Enter the amount and cash given by the customer and know minimum number of notes to return </p>
+      <form>
+        <label> Bill amount </label>
+        <input type="number" onChange={(e) => handleAmtChange(e.target.value)} required />
+        <button hidden={!showCash} onClick={(e) => handleNextClick(e)}> Next </button>
+        <div hidden={showCash} >
+          <label> Cash given </label>
+          <input type="number" onChange={(e) => handleCashChange(e.target.value)} required />
+          <button onClick={(e) => handleCheckClick(e)}> Check </button>
+          <div hidden={showTable} >
+            <h1> Table </h1>
+          </div>
+        </div>
+        <h3> Return change </h3>
+      </form>
     </div>
   );
 }
-
 export default App;
